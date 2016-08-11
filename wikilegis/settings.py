ab17951dd645c6823b43993db84a830585cee430
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-08-11 16:34:57 -0300

    Updated requirements and settings.py
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- wikilegis/settings.py
+++ wikilegis/settings.py
@@ -16,7 +16,7 @@ from django.utils.translation import ugettext_lazy as _
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from decouple import config
-from django_database_url import parse as db_url
+from dj_database_url import parse as db_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 HERE = os.path.dirname(os.path.abspath(__file__))
