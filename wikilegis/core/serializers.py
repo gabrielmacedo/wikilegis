f94047c1daf5e4329bb344b8c9048459b11dd0d7
erivanio <erivanio.vanin@gmail.com>
2016-08-22 13:19:10 -0300

    add create user in API

--- wikilegis/core/serializers.py
+++ wikilegis/core/serializers.py
@@ -133,6 +133,12 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('email', 'first_name', 'last_name', 'avatar')
 
 
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'password')
+
+
 class TypeSegmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = TypeSegment
